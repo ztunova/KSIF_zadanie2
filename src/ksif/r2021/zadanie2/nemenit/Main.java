4fa6178acf74b44c24477466db91b23ae287286d
Zofia Tunova <zofka.tunova@gmail.com>
2021-12-14 20:13:36 +0100

    Initial commit

---
+++ src/ksif/r2021/zadanie2/nemenit/Main.java
@@ -0,0 +1,55 @@
+package ksif.r2021.zadanie2.nemenit;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import ksif.r2021.zadanie2.student.Solver;
+
+import javax.sound.midi.Soundbank;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String pt1 = "duringworldwariiseveralsovietspyringscommunicatedtomoscowcentreusingtwocipherswhichareessentiallyevolutionaryimprovementsonthebasicnihilistcipheraverystrongversionwasusedbymaxclauseninrichardsorgesnetworkinjapanandbyalexanderfooteinthelucyspyringinswitzerlandaslightlyweakerversionwasusedbytherotekapellenetwork";
+        String pt2 = "matsumotogivesthreecouriersasealedpacketeachtodelivertotokyoheinformsthemthatacopyofthetreatyisinsideoneofthemhaghiobtainsallthreepackagesandfindsonlynewspapersbuthehasonemorecarduphissleevematsumotopitieskittyayoungwomanhefindshuddlinginadoorwayduringarainstormandtakesherinwhenhepreparestoleaveforjapanwiththetreatyshebegshimtospendafewhourswithherhegivesinattractedbyherbeautybutwhenhewakesuplatersheisgonewiththetreatydisgracedhecommitsritualsuicidetracksjellusicdowninhishomecountrybutistoolatehaghihasalreadybetrayedhimandwhenconfrontedbyhissuperiorsjellusicshootshimselfwirestheserialnumbersofthebanknotesusedtopayjellusicwhichjasonpassesontoagentnoworkingundercoverasacircusclownnamednemototraceonatraintripoutofthecountryinpursuitofthestolentreatyisnearlykilledinatrapsetbyhaghiwhileheissleepinghiscarisdetachedandleftinatunnelheawakensjustbeforeanothertrainsmashesintoitsonjawhohadbeentrickedintobeingtheonetosmugglethetreatyoutofthecountrybyhaghispromisenottoharmlearnsofthecrashracestothesiteandisreunitedwithherlove";
+        Integer[] perm = {18, 4, 19, 1, 10, 6, 17, 14, 13, 0, 2, 9, 7, 16, 11, 5, 12, 15, 3, 8};
+        String withoutT = "tswhutzwzedckhwmrkkozurdkrhshksbkxlsojkwksomwzxkbdckrwzwzjazmkdfgzrthwmktwmswsozlazgwmkwrkswadhdfhdxkzfkzgwmktmsemdzpwsdfhsbbwmrkklsojsekhsfxgdfxhzfbafkqhlslkrhpuwmkmshzfktzrkosrxulmdhhbkkcktswhutzwzldwdkhjdwwasazufeqztsfmkgdfxhmuxxbdfedfsxzzrqsaxurdfesrsdfhwzrtsfxwsjkhmkrdfqmkfmklrklsrkhwzbksckgzryslsfqdwmwmkwrkswahmkpkehmdtwzhlkfxsgkqmzurhqdwmmkrmkedckhdfswwrsowkxpamkrpksuwapuwqmkfmkqsjkhulbswkrhmkdhezfkqdwmwmkwrkswaxdhersokxmkozttdwhrdwusbhudodxkwrsojhykbbuhdoxzqfdfmdhmztkozufwrapuwdhwzzbswkmsemdmshsbrksxapkwrsakxmdtsfxqmkfozfgrzfwkxpamdhhulkrdzrhykbbuhdohmzzwhmdthkbgqdrkhwmkhkrdsbfutpkrhzgwmkpsfjfzwkhuhkxwzlsaykbbuhdoqmdomyshzflshhkhzfwzsekfwfzqzrjdfeufxkrozckrshsodrouhobzqffstkxfktzwzwrsokzfswrsdfwrdlzuwzgwmkozufwradflurhudwzgwmkhwzbkfwrkswadhfksrbajdbbkxdfswrslhkwpamsemdqmdbkmkdhhbkkldfemdhosrdhxkwsomkxsfxbkgwdfswuffkbmksqsjkfhyuhwpkgzrksfzwmkrwrsdfhtshmkhdfwzdwhzfysqmzmsxpkkfwrdojkxdfwzpkdfewmkzfkwzhtueebkwmkwrkswazuwzgwmkozufwrapamsemdhlrztdhkfzwwzmsrtbksrfhzgwmkorshmrsokhwzwmkhdwksfxdhrkufdwkxqdwmmkrbzck";
+        try {
+            // init
+            String ct1 = new String(Files.readAllBytes(Paths.get("msg_short.txt")));
+            String ct2 = new String(Files.readAllBytes(Paths.get("msg_long.txt")));
+            Solver s = new Solver();
+
+            // 1. vigenere
+            // vieme ze na konci ZT je "bordel" co rusi statistiky
+            // porovname po vylusteni len PT cast bez kluca
+            String pt1cand = s.solveVigenere(ct1);
+            System.out.println("4b - " + pt1.equals(pt1cand.substring(0, pt1cand.length() - 20)));
+            // 2. inv perm
+            // zoberieme z predch. ulohy kluc
+            // prevedieme pismena na permutaciu (do toho tvaru s cim robi nasa Transpozicia)
+            // porovname permutaciu
+            String transpKey = pt1cand.substring(pt1cand.length() - 20, pt1cand.length());
+            Integer[] permutationCand = s.getPermutation(transpKey);
+            System.out.println("2b - " + Arrays.toString(perm).equals(Arrays.toString(permutationCand)));
+            // 3. remove transp
+            // desifrujeme jednu clast zlozenej sifry
+            // porovname vysledok
+            String tRemoved = s.solveTransposition(ct2, perm);
+            System.out.println("3b - " + withoutT.equals(tRemoved));
+            // 4. 
+            // desifrujeme druhu cast zlozenej sifry
+            // porovname vysledok
+            String pt2cand = s.solveSubstitution(tRemoved);
+            System.out.println("6b - " + pt2.equals(pt2cand));
+
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+}
---
+++ src/ksif/r2021/zadanie2/student/Solver.java
@@ -0,0 +1,39 @@
+package ksif.r2021.zadanie2.student;
+
+public class Solver {
+
+    public String solveVigenere(String ct1) {
+        String retVal = null;
+        // hint: hodnotit bez konca, t.j. kluca
+        //   ...     
+        //
+        //return "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        return retVal;
+    }
+
+    public Integer[] getPermutation(String key) {
+        Integer[] perm = new Integer[key.length()];
+        //
+        //   ...     
+        //
+        return perm;
+    }
+
+    public String solveTransposition(String ct2, Integer[] key) {
+        String retVal = null;
+        //
+        //   ...     
+        //
+        return retVal;
+    }
+
+    public String solveSubstitution(String ct2WithoutT) {
+        String retVal = null;
+        //
+        //   ...     
+        //
+        return retVal;
+    }
+
+    
+}
